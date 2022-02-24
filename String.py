c4eb67823d0ccadaa6b81ddf5fa76df6daade01d
Ekaterina Tcoi <90351053+EkaterinaTcoi@users.noreply.github.com>
2022-02-24 13:47:25 +0000

    Add files via upload

---
+++ String.py
@@ -0,0 +1,17 @@
+name = "Bob"
+age = 25
+string = "Hi my name is %s and I amd %i years old!"%(name, age)
+print(string)
+
+name = "Bob"
+string1 = f"Hello, {name}"
+print(string1)
+
+string2 = f"1+1 is {1+1}"
+print(string2)
+
+
+
+
+
+
